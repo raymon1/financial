a9da12cad6d1cb47457f2e64d5a47cf7816c3f9d
Raymon mina <raymonmina@gmail.com>
2020-07-30 18:54:25 +0200

    [TEST] Adds tests for XNPV and XIRR against excel

--- tests/test.rs
+++ tests/test.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
 
+    use chrono::{Utc, DateTime, NaiveDate};
     use financial;
 
     pub const PRECISION: f64 = 1e-7;
@@ -16,7 +17,7 @@ mod tests {
                 test_case.map(|x| x.parse::<f64>().unwrap()).collect());
             
             let res = financial::npv(r, &values);
-            assert!((ans - res).abs() < PRECISION, "case {}, answer is {}, result is {}", case_index, ans, res);
+            assert!((ans - res).abs() < PRECISION, "case {}: answer is {}, result is {}", case_index, ans, res);
         });
     }
 
@@ -31,10 +32,64 @@ mod tests {
             
             let res = financial::irr(&values, None).unwrap_or_else(|_| f64::NAN);
 
-            if !(ans.is_nan() ) {
-                assert!((ans - res).abs() < PRECISION, "cases {}, answer is {}, result is {}", case_index, ans, res);
-            }
+            if !ans.is_nan() {
+                assert!((ans - res).abs() < PRECISION, "case {}: answer is {}, result is {}", case_index, ans, res);
+            } 
         });
+    }
+
+    #[test]
+    fn xnpv() {
+        test_fn("./tests/test_data/xnpv.csv", |test_case, case_index| {
+            let mut test_case = test_case;
+
+            let date_from_str = |x| DateTime::<Utc>::from_utc(
+                NaiveDate::parse_from_str(x, "%m/%d/%Y").unwrap().and_hms(0, 0, 0), Utc);
+
+            let(ans, r, values, dates) : (f64, f64, Vec<f64>, Vec<DateTime<Utc>>) = (
+                test_case.next().unwrap().parse::<f64>().unwrap(),
+                test_case.next().unwrap().parse::<f64>().unwrap(),
+                test_case.by_ref().take_while(|x| x.parse::<f64>().is_ok()).map(|x| x.parse::<f64>().unwrap()).collect(), 
+                test_case.map(|x| date_from_str(x)).collect()
+            );
+            
+            let res = financial::xnpv(r, &values, &dates).unwrap()  ;
+            assert!((ans - res).abs() < PRECISION, "case {}: answer is {}, result is {}", case_index, ans, res);
+        });
+    }
+
+    #[test]
+    fn xirr() {
+        test_fn("./tests/test_data/xirr.csv", |test_case, case_index| {
+            let mut test_case = test_case;
+
+            let date_from_str = |x| DateTime::<Utc>::from_utc(
+                NaiveDate::parse_from_str(x, "%m/%d/%Y").unwrap().and_hms(12, 0, 0), Utc);
+
+            let(ans, values, dates) : (f64, Vec<f64>, Vec<DateTime<Utc>>) = (
+                test_case.next().unwrap().parse::<f64>().unwrap_or_else(|_| f64::NAN),
+                test_case.by_ref().take_while(|x| x.parse::<f64>().is_ok()).map(|x| x.parse::<f64>().unwrap()).collect(), 
+                test_case.map(|x| date_from_str(x)).collect()
+            );
+            
+            let res = financial::xirr(&values, &dates, None).unwrap_or_else(|_| f64::NAN);
+
+            if !res.is_nan() {
+                assert_nearly_eq(0., financial::xnpv(res, &values, &dates).unwrap(), case_index);
+            }
+
+            // It appears that excel has a flow in its XIRR function that cause the answer to converge at 0.000000002980
+            // That's why I preferred to use the XNPV function to make sure that the output of xirr converge the NPV value of cashflow to approximately zero,
+            // It's also good to note that this is not perfect since XIRR is dependent on XNPV that is used to test it and assumes the correctness of XNPV function.
+            
+            // if !ans.is_nan() {
+            //     assert!((ans - res).abs() < PRECISION, "case {}: answer is {}, result is {}, diff is {}", case_index, ans, res, (ans - res).abs());
+            // }
+        });
+    }
+
+    fn assert_nearly_eq(ans: f64, res: f64, case_index: i32) {
+        assert!((ans - res).abs() < PRECISION, "case {}: answer is {}, result is {}, diff is {}", case_index, ans, res, (ans - res).abs());
     }
 
 
